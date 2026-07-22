dbe0902c75f4eb2aedfd5838de29ad521538b114
Ayush Kumar Tiwary <tiwaryayush1208@gmail.com>
2026-07-22 18:57:53 +0530

    Initial Setup

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Resume Intelligence",
+    page_icon="📄",
+    layout="wide"
+)
+
+st.title("AI-Powered Resume Intelligence and Career Recommendation System")
+
+st.success("Development Environment Setup Successful ✅")
+
+st.write("Welcome to the project!")
